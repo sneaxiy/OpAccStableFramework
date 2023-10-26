88048384f61f6cd77b09c8773448cc5cda9585f7
yangxiaoyu14 <yangxiaoyu14@baidu.com>
2023-10-26 09:03:47 +0000

    gelu_test

--- tests/gelu_test.py
+++ tests/gelu_test.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,22 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from op_acc_stable_run import check_tensor_diff, op_acc_stable_run
 
-
 class GeluTest:
-    def set_configs_0(self, paddle):
-        self.shape = [1, 12288]
-        self.dtype = "float32"
-        self.inputs = {
-            "x": paddle.randn(self.shape, dtype=self.dtype),
-            "y_grad": paddle.randn(self.shape, dtype=self.dtype),
-        }
-    
-    def set_configs_1(self, paddle):
-        self.shape = [1,  4096, 24576]
-        self.dtype = "float32"
+    def __init__(self, shape, dtype):
+        self.shape = shape
+        self.dtype = dtype
+
+    def set_configs(self, paddle):
         self.inputs = {
             "x": paddle.randn(self.shape, dtype=self.dtype),
             "y_grad": paddle.randn(self.shape, dtype=self.dtype),
@@ -50,6 +43,10 @@ class GeluTest:
         for pd, th in zip(pd_ret, th_ret):
             check_tensor_diff(pd, th, atol=1e-6, rtol=1e-6)
 
-
 if __name__ == "__main__":
-    op_acc_stable_run(GeluTest)
+    op_acc_stable_run(GeluTest(shape=[1, 12288], dtype="float32"))
+    op_acc_stable_run(GeluTest(shape=[1, 12288], dtype="float16"))
+    op_acc_stable_run(GeluTest(shape=[1, 12288], dtype="bfloat16"))
+    op_acc_stable_run(GeluTest(shape=[1, 4096, 24576], dtype="float32"))
+    op_acc_stable_run(GeluTest(shape=[1, 4096, 24576], dtype="float16"))
+    op_acc_stable_run(GeluTest(shape=[1, 4096, 24576], dtype="bfloat16"))
